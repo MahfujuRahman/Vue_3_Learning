9e44bb19ac2aba822e8361e2923684926c5a5e0e
MahfujuRahman <mahfujur15-14276@diu.edu.bd>
2024-08-20 12:31:47 +0600

    Fundamental of View. Options Api and Composition Api.

---
+++ src/CompositionApiLong.vue
@@ -0,0 +1,46 @@
+<!-- Composition api Long-->
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup () {
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['Task 1', 'Task 2', 'Task 3']);    
+
+    const toggleStatus = () => {
+      if (status.value === 'active') {
+       status.value = 'pending';
+      } else if(status.value === 'pending'){
+       status.value = 'inactive';
+      } else{
+       status.value = 'active';
+      }
+    }
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p v-if="status === 'active'">User is Active.</p>
+    <p v-else-if="status === 'pending'">User is pending.</p>
+    <p v-else>User is Inactive</p>
+
+    <h3>Tasks:</h3>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+   <br>
+    <button @click="toggleStatus">Change Status</button>
+  </div>
+</template>
+
---
+++ src/OptionsApi.vue
@@ -0,0 +1,46 @@
+<!-- Options api -->
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World',
+      status: "active",
+      tasks:['Task 1', 'Task 2', 'Task 3'],
+      link:'https://google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if(this.status === 'pending'){
+        this.status = 'inactive';
+      } else{
+        this.status = 'active';
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p v-if="status === 'active'">User is Active.</p>
+    <p v-else-if="status === 'pending'">User is pending.</p>
+    <p v-else>User is Inactive</p>
+
+    <h3>Tasks:</h3>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+    <a :href="link">Click for google</a>
+
+    <br>
+
+    <button v-on:click="toggleStatus">Change Status</button>
+    <button @click="toggleStatus">Change Status</button>
+  </div>
+</template>
+
