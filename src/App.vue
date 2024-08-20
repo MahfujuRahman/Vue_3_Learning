d7dc73827c07705550871f3ee5fbe98160ef5789
MahfujuRahman <mahfujur15-14276@diu.edu.bd>
2024-08-20 16:03:08 +0600

    "Added vue-router dependency and configured it in main.js, updated App.vue to use RouterView, and modified package.json and package-lock.json accordingly." Router Setip done Initially for home page.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
 import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
 
   <Navbar />
-  <!-- For Hero component we can use two props as title and subtitle, otherwise default value will be shown!-->
-  <Hero /> 
-  <HomeCards /> 
-  <JobListings :limit="3" showButton="true"/> 
-
+  <RouterView />
+  
 </template>
 
