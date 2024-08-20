9a695f0bb639d9057ba3e981bdc7dacd6be46dae
MahfujuRahman <mahfujur15-14276@diu.edu.bd>
2024-08-20 21:33:49 +0600

    "Added vue-toastification dependency and implemented toast notifications in AddJobView"

--- src/view/AddJobView.vue
+++ src/view/AddJobView.vue
@@ -2,6 +2,7 @@
 import { reactive } from 'vue';
 import router from '@/router';
 import axios from 'axios';
+import { useToast } from 'vue-toastification';
 
 const form = reactive({
     type: "Full-Time",
@@ -16,6 +17,8 @@ const form = reactive({
         contactPhone: "",
     }
 });
+
+const toast = useToast();
 
 const handleSubmit = async () => {
     const newJob = {
@@ -34,10 +37,12 @@ const handleSubmit = async () => {
     try {
         const response = await axios.post('/api/jobs', newJob);
         //@todo - show toast
+        toast.success('Job Added Successfully');
         router.push(`/jobs/${response.data.id}`);
         
     } catch (error) {
         //@todo - show toast
+        toast.error('Job was not Added')
         console.error('Error fetching Job', error);
     }
 };
