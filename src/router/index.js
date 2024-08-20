3f6bc741ba169e54d066cb857982cf259b76246d
MahfujuRahman <mahfujur15-14276@diu.edu.bd>
2024-08-20 16:38:15 +0600

    "Added NotFoundView route to router"

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/view/HomeView.vue";
 import JobsView from "@/view/jobsView.vue";
+import NotFoundView from "@/view/NotFoundView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: "/jobs",
             name: "jobs",
             component: JobsView,
+        },
+        {
+            path: "/:catchAll(.*)",
+            name: "NotFound",
+            component: NotFoundView,
         }
     ],
 });
