1cde3b2d5e6538916dd4e651fc937d0e72740b24
MahfujuRahman <mahfujur15-14276@diu.edu.bd>
2024-08-20 22:20:23 +0600

    Completed Crud and now you can clone this repo and view this project.

---
+++ src/components/JobListings.vue
@@ -0,0 +1,82 @@
+<script setup>
+// import jobData from "@/jobs2.json";
+import { reactive, defineProps,onMounted } from "vue";
+import JobListing from "./JobListing.vue";
+import { RouterLink } from "vue-router";
+import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+/**
+ * using it for json file
+ * This component represents a list of job listings.
+ *
+ * @component JobListings
+ * @file /d:/Practice/Vue3/vue-crash-TraversyMedia/vue_crash/src/components/JobListings.vue
+ *
+ * @param {ref} jobs - A reactive reference to the job data.
+ */
+// const jobs = ref(jobData);
+
+// use ref instead of reactive for server
+// const jobs = ref([]);
+
+// onMounted(async () => {
+//     try {
+//         const response = await axios.get("http://localhost:5000/jobs");
+//         jobs.value = response.data;
+//     } catch (error) {
+//         console.error('Error fetching Jobs' , error);
+//     }
+// });
+
+// Use reactive instead of ref for server
+const state = reactive({
+    jobs: [],
+    isLoading: true,
+});
+
+onMounted(async () => {
+    try {
+        const response = await axios.get("/api/jobs");
+        state.jobs = response.data;
+    } catch (error) {
+        console.error('Error fetching Jobs' , error);
+    } finally{
+        state.isLoading = false;
+    }
+});
+
+
+
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h2>
+            <!-- Pulse Loader -->
+             <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader color="#38a169" size="10px" />
+             </div>
+             <!-- show job lising when done loading -->
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
+            </div>
+        </div>
+    </section>
+
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+        <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
+            All Jobs</RouterLink>
+    </section>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/view/HomeView.vue";
+import JobsView from "@/view/jobsView.vue";
+import NotFoundView from "@/view/NotFoundView.vue";
+import JobView from "@/view/JobView.vue";
+import AddJobView from "@/view/AddJobView.vue";
+import EdiJobView from "@/view/EdiJobView.vue";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes:[
+        {
+            path: "/",
+            name: "Home",
+            component: HomeView,
+        },
+        {
+            path: "/jobs",
+            name: "jobs",
+            component: JobsView,
+        },
+        {
+            path: "/jobs/:id",
+            name: "job",
+            component: JobView,
+        },
+        {
+            path: "/jobs/add",
+            name: "add-job",
+            component: AddJobView,
+        },
+        {
+            path: "/jobs/edit/:id",
+            name: "edit-job",
+            component: EdiJobView,
+        },
+        {
+            path: "/:catchAll(.*)",
+            name: "NotFound",
+            component: NotFoundView,
+        },
+    ],
+});
+
+export default router;
+
+
